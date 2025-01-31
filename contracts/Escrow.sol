346ce74e0ccc3474af01145f94f02c2de3ebb0ef
trombitassandor <trombitassandor1@gmail.com>
2025-01-31 15:03:49 +0200

    Implement Escrow and RealEstate contracts with listing functionality and metadata

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,13 +2,50 @@
 pragma solidity ^0.8.0;
 
 interface IERC721 {
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _id
-    ) external;
+    function transferFrom(address _from, address _to, uint256 _id) external;
 }
 
 contract Escrow {
+    address public nftAddress;
+    address payable public seller;
+    address public inspector;
+    address public lender;
 
+    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => address) public buyer;
+    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => uint256) public escrowAmount;
+
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only seller can call this function");
+        _;
+    }
+
+    constructor(
+        address _nftAddress,
+        address payable _seller,
+        address _inspector,
+        address _lender
+    ) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+    }
+
+    function list(
+        uint256 _tokenId,
+        address _buyer,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount
+    ) public payable onlySeller {
+        // transfer nft from seller to this escrow contract
+        // must be approved first
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _tokenId);
+
+        isListed[_tokenId] = true;
+        buyer[_tokenId] = _buyer;
+        purchasePrice[_tokenId] = _purchasePrice;
+        escrowAmount[_tokenId] = _escrowAmount;
+    }
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -6,5 +6,122 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+    let buyer, seller, inspector, lender;
+    let realEstate, escrow;
+    let firstTokenId = 1;
+    let purchasePrice = tokens(10);
+    let escrowAmount = tokens(2);
+    let isConsoleLogEnabled = false;
 
+    beforeEach(async () => {
+        // get signers
+        const signers = await ethers.getSigners();
+        // log signers (for debugging)
+        //consoleLog("Signers");
+        //consoleLog(signers);
+
+        // setup accounts
+        [buyer, seller, inspector, lender] = signers;
+        consoleLog("Buyer = " + buyer.address +
+            "\n" + "Seller = " + seller.address +
+            "\n" + "Inspector = " + inspector.address +
+            "\n" + "Lender = " + lender.address);
+
+        // deploy real estate contract
+        const RealEstate = await ethers.getContractFactory('RealEstate');
+        realEstate = await RealEstate.deploy();
+        consoleLog("Real estate contract address = " + realEstate.address);
+
+        // mint token
+        // https://bafybeignot2h6boaci4eqer6irzibmv3xn6qcfzmeyszddnyus5fowbtgu.ipfs.w3s.link/RealEstate00.json
+        // the ipfs format
+        const tokenURI = "https://ipfs.io/ipfs/bafybeignot2h6boaci4eqer6irzibmv3xn6qcfzmeyszddnyus5fowbtgu/RealEstate00.json";
+        const mintTransaction = await realEstate.connect(seller).mint(tokenURI);
+        await mintTransaction.wait();
+        consoleLog("Minted token with URI = " + tokenURI);
+
+        // deploy escrow contract
+        const Escrow = await ethers.getContractFactory('Escrow');
+        escrow = await Escrow.deploy(
+            realEstate.address,
+            seller.address,
+            inspector.address,
+            lender.address);
+        consoleLog("Escrow contract address = " + escrow.address);
+
+        // approve to transfer token721/real estate from seller to escrow
+        approveTransaction = await realEstate.connect(seller).approve(escrow.address, firstTokenId);
+        await approveTransaction.wait();
+
+        // transfer token721/real estate from seller to escrow
+        // list token712/real estate for sale
+        listTransaction = await escrow.connect(seller).list(
+            firstTokenId, buyer.address, purchasePrice, escrowAmount);
+        await listTransaction.wait();
+    })
+
+    describe('Deployment', () => {
+        it("Check NFT address", async () => {
+            const resultNftAddress = await escrow.nftAddress();
+            consoleLog("Escrow NFT address = " + resultNftAddress);
+            expect(resultNftAddress).to.equal(realEstate.address);
+        })
+
+        it("Check seller address", async () => {
+            const resultSeller = await escrow.seller();
+            consoleLog("Escrow seller address = " + resultSeller);
+            expect(resultSeller).to.equal(seller.address);
+        })
+
+        it("Check inspector address", async () => {
+            const resultInspector = await escrow.inspector();
+            consoleLog("Escrow inspector address = " + resultInspector);
+            expect(resultInspector).to.equal(inspector.address);
+        })
+
+        it("Check lender address", async () => {
+            const resultLender = await escrow.lender();
+            consoleLog("Escrow lender address = " + resultLender);
+            expect(resultLender).to.equal(lender.address);
+        })
+    })
+
+    describe('Listing', () => {
+        it('Check ownership', async () => {
+            const resultOwner = await realEstate.ownerOf(firstTokenId);
+            expect(resultOwner).to.equal(escrow.address);
+            consoleLog("Ownership updated to escrow contract. Escrow contract owns first token.");
+            consoleLog("Escrow contract owns first token");
+        });
+
+        it('Check listing status', async () => {
+            const resultIsListed = await escrow.isListed(firstTokenId);
+            expect(resultIsListed).to.equal(true);
+            consoleLog("First token / real estate is listed for sale.");
+        });
+
+        it('Check escrow buyer', async () => {
+            const resultBuyer = await escrow.buyer(firstTokenId);
+            expect(resultBuyer).to.equal(buyer.address);
+            consoleLog("Check escrow buyer = " + resultBuyer);
+        });
+
+        it('Check escrow purchase price', async () => {
+            const resultPurchasePrice = await escrow.purchasePrice(firstTokenId);
+            expect(resultPurchasePrice).to.equal(purchasePrice);
+            consoleLog("Check escrow purchase price = " + resultPurchasePrice);
+        });
+
+        it('Check escrow amount', async () => {
+            const resultEscrowAmount = await escrow.escrowAmount(firstTokenId);
+            expect(resultEscrowAmount).to.equal(escrowAmount);
+            consoleLog("Check escrow amount = " + resultEscrowAmount);
+        });
+    });
+
+    function consoleLog(message) {
+        if (isConsoleLogEnabled) {
+            console.log(message);
+        }
+    }
 })
