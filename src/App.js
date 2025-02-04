cd63b6a02ddfae6f8d00b9051b85c30eb6a3d177
trombitassandor <trombitassandor1@gmail.com>
2025-02-04 15:41:56 +0200

    Update welcome message; Placeholder searchbar; Placeholder card

--- src/App.js
+++ src/App.js
@@ -38,10 +38,30 @@ function App() {
     <div>
 
       <Navigation account={account} setAccount={setAccount} />
+      <Search/>
 
       <div className='cards__section'>
 
-        <h3>Welcome to Millow!</h3>
+        <h3>Welcome to Web3RealEstateApp!</h3>
+
+        <hr/>
+
+        <div className='cards'>
+          <div className='card'>
+            <div className='card__image'>
+              <img src="" alt="Home"/>
+            </div>
+            <div className="card__info">
+              <h4>1 ETH</h4>
+              <p>
+                <strong>2</strong> bedrooms |
+                <strong>1</strong> bathrooms |
+                <strong>60</strong> sqm
+              </p>
+              <p>Street [street_name] Nr[street_number]</p>
+            </div>
+          </div>
+        </div>
 
       </div>
 
